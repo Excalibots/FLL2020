69d70bdfc02bfe24f17a5f776f2566813af322cf
21Jephy <niravsurabhi@gmail.com>
2021-01-18 15:09:02 -0500

    run 1 and 2 optimized

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ from run3 import *
 #set up the runs here
 
 print('Run 1')
-#run_one()
+# run_one()
 
 print('Run 2')
-# Bench_Scotch()
+Bench_Scotch()
 
 print('Run 2a')
-do_step_tracker()
-go_back_from_step_tracker()
-ready_treadmill()
-do_treadmill()
-back_from_treadmill()
-do_rower()
+# do_step_tracker()
+# go_back_from_step_tracker()
+# ready_treadmill()
+# do_treadmill()
+# back_from_treadmill()
+# do_rower()
 
 print('Run 3')
 #run3_b()
 
+print('Tests')
 #print('Tests')
 #while True:
 #test()s
